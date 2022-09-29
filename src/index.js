4004d48dbc89d23ecb1bf90275b51d9b1242afcf
dishap-dk <dishapushpad4@gmail.com>
2022-09-29 12:18:09 +0530

    basic module

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express=require("express")
+const mongoose=require("mongoose")
+const route=require("./route/route")
+const app=express()
+
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://Dishap:gn6kyXLuhnBE1EJK@cluster0.bwp65jf.mongodb.net/group40Database?retryWrites=true&w=majority")
+.then(() => console.log("MongoDB is connected"))
+.catch( err => console.log(err))
+
+app.use('/', route);
+
+
+app.listen(process.env.PORT||3000 , function() {
+    console.log('Express app running on port ' + (process.env.PORT || 3000 ))
+});
+
+
---
+++ src/route/route.js
@@ -0,0 +1,14 @@
+const express=require('express')
+const route=express.Router()
+
+
+
+
+
+
+
+
+
+
+
+module.exports=route
